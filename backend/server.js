bcdae40025a7e7c2598c28df10affaad59c69e6b
Shahabas Mohammed <shahabasg1@gmail.com>
2023-12-15 14:03:30 +0530

    path 2

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,10 @@ app.use('/api/admin',adminRoutes)
 
 // if(process.env.NODE_ENV == 'production'){
     const __dirname=path.resolve()
-    app.use(express.static(path.join(__dirname,'../frontend/dist')))
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','dist','index.html')))
+    app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html')))
+
 // }else{
 //    app.get('/',(req,res)=>res.send('server is ready'))  
 // } 
