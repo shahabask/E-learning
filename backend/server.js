73edbb3cb38ba084e6e310412f22e75d21c93eb4
Shahabas Mohammed <shahabasg1@gmail.com>
2023-12-15 13:57:49 +0530

    path correction

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/admin',adminRoutes)
 
 // if(process.env.NODE_ENV == 'production'){
     const __dirname=path.resolve()
-    app.use(express.static(path.join(__dirname,'frontend/dist')))
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
     app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','dist','index.html')))
 // }else{
 //    app.get('/',(req,res)=>res.send('server is ready'))  
